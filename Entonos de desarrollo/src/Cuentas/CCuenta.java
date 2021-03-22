8c3e5e1b15576ec479bd97afa68a1a7bef558dc9
FullTexas32 <81188027+FullTexas32@users.noreply.github.com>
2021-03-22 21:42:46 +0100

    Update CCuenta.java
    Actualizado

--- Entonos de desarrollo/src/Cuentas/CCuenta.java
+++ Entonos de desarrollo/src/Cuentas/CCuenta.java
@@ -1,22 +1,25 @@
-package Cuentas;
-
 public class CCuenta {
 
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInter�s;
+    private double tipoInterÃ©s;
+    public getnombre() {return nombre;}
+	public getcuenta() {return cuenta;}
+	public getsaldo() {return saldo; }
+
 
     public CCuenta()
     {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
+    public CCuenta(String nom, String cue, double sal, double tipo, float cantidad)
     {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    	public getnombre() {nombre=nom;}
+    	public getcuenta() {cuenta=cue;}
+    	public getsaldo() {saldo=sal; }
+    	float cantidad;
     }
 
     public double estado()
@@ -39,9 +42,25 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         saldo = saldo - cantidad;
     }
-    
-    public void operativa_cuenta()
-    {
+    public operativa_cuenta {
     	
-    }
+            CCuenta cuenta1;
+            double saldoActual;
+
+            cuenta1 = new CCuenta("Antonio LÃ³pez","1000-2365-85-1230456789",2500,0);
+            saldoActual = cuenta1.estado();
+            System.out.println("El saldo actual es"+ saldoActual );
+
+            try {
+                cuenta1.retirar(2300);
+            } catch (Exception e) {
+                System.out.print("Fallo al retirar");
+            }
+            try {
+                System.out.println("Ingreso en cuenta");
+                cuenta1.ingresar(695);
+            } catch (Exception e) {
+                System.out.print("Fallo al ingresar");
+            }
+        }   
 }
